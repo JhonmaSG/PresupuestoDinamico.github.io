932a4119d37bc4379b7e3d52edaa30e1af0c170f
JhonmaSG <jmserrnaog@udistrital.edu.co>
2024-12-10 01:54:16 -0500

    completed heads

--- js/Ingreso.js
+++ js/Ingreso.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Dato } from './Dato.js';
 export class Ingreso extends Dato{
     static contadorIngresos = 0;
@@ -9,16 +8,4 @@ export class Ingreso extends Dato{
     get id(){
         return this._id;
     }
-=======
-import { Dato } from './Dato.js';
-export class Ingreso extends Dato{
-    static contadorIngresos = 0;
-    constructor(descripcion, valor){
-        super(descripcion, valor);
-        this._id = ++Ingreso.contadorIngresos;
-    }
-    get id(){
-        return this._id;
-    }
->>>>>>> 04662ee232ffa3c60c4da51bf2c8f1f086837bb0
 }
